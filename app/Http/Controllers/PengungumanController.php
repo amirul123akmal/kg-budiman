d2eabcd99b286d90b2f1873d690948dddb8c4f33
IzzahSN <izzahfsn@gmail.com>
2025-11-08 13:10:53 +0800

    `Added CRUD methods to PengungumanController`

--- app/Http/Controllers/PengungumanController.php
+++ app/Http/Controllers/PengungumanController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class PengungumanController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.pengunguman.index');
+    }
+
+    public function create()
+    {
+        return view('admin.pengunguman.create');
+    }
+
+    public function edit($id)
+    {
+        return view('admin.pengunguman.edit', compact('id'));
+    }
+
+    public function store(Request $request)
+    {
+        // Logic to store the announcement
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update the announcement
+    }
+
+    public function destroy($id)
+    {
+        // Logic to delete the announcement
+    }
 }
